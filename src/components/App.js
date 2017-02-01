9321e8e2a97f02aea2b4dd83bd843b469e273166
Brian Bycynski <high5bri@hotmail.com>
2017-02-01 15:02:04 -0500

    added react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
-
+// import Home from './Home'
+import { Link } from 'react-router'
 export default class App extends Component {
 
+  static propTypes = {
+    children: React.PropTypes.element.isRequired
+  }
   render () {
     return <div>
       <h1>Simple SPA</h1>
       <ul className='header' />
-      <li>Home</li>
+      <li><Link to='/'>Home</Link></li>
       <li>Stuff</li>
       <li>Contact</li>
-      <div className='content' />
+      <div className='content'>
+        {this.props.children}
+      </div>
     </div>
   }
 }
--- src/components/Rooter.js
+++ src/components/Rooter.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-import { Router, Route } from 'react-router'
+import { Router, Route, IndexRoute } from 'react-router'
+import Home from './Home'
 import App from './App'
 export default class Rooter extends Component {
   render () {
     return <Router>
       <Route path='/' component={App} />
+      <IndexRoute component={Home} />
     </Router>
   }
 }
