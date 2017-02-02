bccba21c2190bfcf6d2f4d31cf41109ab4a20025
Brian Bycynski <high5bri@hotmail.com>
2017-02-02 13:22:35 -0500

    React Router

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ export default class Home extends Component {
     return <div>
       <h2>Hello Home</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <img src='http://s3.amazonaws.com/assets.prod.vetstreet.com/7b/0526609e8c11e0a2380050568d634f/file/Beagle-3-645mk062311.jpg' alt='' width='550' height='250' />
     </div>
   }
 }
